b7ff6d6feca394f8e965d0a86e35ad62dee655cd
Andy <Frank1126.lin@gmail.com>
2025-03-07 21:38:36 +0800

    Update real_estate_spider.py
    update the new pages total and keywords

--- real_estate_spider.py
+++ real_estate_spider.py
@@ -11,6 +11,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
 
+
+keys = "东望"
+area = "吴中区"
 # 解析网页内容并提取房屋信息
 def parse_page(table):
     rows = table.find_elements(By.TAG_NAME, "tr")
@@ -32,20 +35,30 @@ def main():
     browser = webdriver.Chrome()
     try:
         browser.get('http://spf.szfcweb.com/szfcweb/DataSerach/CanSaleHouseSelectIndex.aspx')
+        
+        input = browser.find_element(By.NAME,'ctl00$MainContent$txt_Pro')
+        input.send_keys(keys)
+        time.sleep(random.uniform(1, 3))
+
         select_element = browser.find_element(By.ID,'MainContent_ddl_qy')
         select = Select(select_element)
-        select.select_by_visible_text('相城区')
+        select.select_by_visible_text(area)
         time.sleep(random.uniform(1, 3))
-        input = browser.find_element(By.NAME,'ctl00$MainContent$txt_Pro')
-        input.send_keys('大悦')
-        time.sleep(random.uniform(1, 3))
+        
         button = browser.find_element(By.ID,'MainContent_bt_select')
         button.click() 
         wait = WebDriverWait(browser, 10)
         time.sleep(random.uniform(1, 3))
 
+
+        pages_total = browser.find_element(By.ID,"MainContent_PageGridView1_PageList_0")
+        page_select = Select(pages_total)
+
+        max_page = page_select.options[-1].text
+        max_page = int(max_page)
+        print(max_page)
         data = []
-        for i in range(9):
+        for i in range(max_page):
             table = browser.find_element(By.ID, "MainContent_PageGridView1")
             houses = parse_page(table)
             for x in houses:
@@ -56,7 +69,7 @@ def main():
             next_button.click()
         # 将data存储到df中
         df = pd.DataFrame(data)
-        df.to_csv('data-{}.csv'.format(time.strftime("%Y%m%d%H%M%S")), index=False)
+        df.to_csv('data-dongwang-{}.csv'.format(time.strftime("%Y%m%d%H%M%S")), index=False)
 
     finally:
         browser.close()
