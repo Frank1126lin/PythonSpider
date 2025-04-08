893998520d0707a3ee25c5cd4d922079ffba9a9f
Andy <Frank1126.lin@gmail.com>
2025-04-08 15:44:44 +0800

    Update realestate_spider_chrome.py

--- realestate_spider_chrome.py
+++ realestate_spider_chrome.py
@@ -5,11 +5,8 @@ import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.ui import Select
-
-
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # 解析网页内容并提取房屋信息
@@ -30,23 +27,27 @@ def parse_page(table):
 
 
 def browser_using(browser, keys, area):
-    
-    input = browser.find_element(By.NAME,'ctl00$MainContent$txt_Pro')
+    wait_input = WebDriverWait(browser, 5)
+    input = browser.find_element(By.NAME, 'ctl00$MainContent$txt_Pro')
+    input.clear()
     input.send_keys(keys)
     time.sleep(random.uniform(1, 3))
 
-    select_element = browser.find_element(By.ID,'MainContent_ddl_qy')
+    wait_select = WebDriverWait(browser, 5)
+    select_element = browser.find_element(By.ID, 'MainContent_ddl_qy')
     select = Select(select_element)
     select.select_by_visible_text(area)
     time.sleep(random.uniform(1, 3))
 
-    button = browser.find_element(By.ID,'MainContent_bt_select')
+    wait_button = WebDriverWait(browser, 5)
+    button = browser.find_element(By.ID, 'MainContent_bt_select')
     button.click()
     # wait = WebDriverWait(browser, 10)
     time.sleep(random.uniform(2, 5))
 
-
-    pages_total = browser.find_element(By.ID,"MainContent_PageGridView1_PageList_0")
+    wait_total = WebDriverWait(browser, 5)
+    pages_total = browser.find_element(
+        By.ID, "MainContent_PageGridView1_PageList_0")
     page_select = Select(pages_total)
 
     max_page = page_select.options[-1].text
@@ -60,25 +61,27 @@ def browser_using(browser, keys, area):
             print(x)
             data.append(x)
         time.sleep(random.uniform(1, 3))
-        next_button = browser.find_element(By.ID, "MainContent_PageGridView1_Next_0")
+        next_button = browser.find_element(
+            By.ID, "MainContent_PageGridView1_Next_0")
         next_button.click()
     # 将data存储到df中
     df = pd.DataFrame(data)
     df.to_csv(f'data-{keys}-{time.strftime("%Y%m%d%H%M%S")}.csv', index=False)
-       
 
 
 if __name__ == '__main__':
     q = {
-        "大悦":"相城区", 
-        "江南":"相城区",
-        "墨香":"相城区",
-        "东望":"吴中区",
-        "融悦":"吴中区",
+        "大悦": "相城区",
+        "江南": "相城区",
+        "墨香": "相城区",
+        "东望": "吴中区",
+        "融悦": "吴中区",
     }
-    
+
     browser = webdriver.Chrome()
-    browser.get('http://spf.szfcweb.com/szfcweb/DataSerach/CanSaleHouseSelectIndex.aspx')
+    browser.get(
+        'http://spf.szfcweb.com/szfcweb/DataSerach/CanSaleHouseSelectIndex.aspx')
+    wait = WebDriverWait(browser, 5)
     for keys, area in q.items():
         print(f"{keys}--{area}")
         try:
